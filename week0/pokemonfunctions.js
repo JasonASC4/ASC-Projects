1bc9ac9a1328e2d2c666ab55eea327d018602533
JasonASC4 <Jasonhr781@gmail.com>
2017-07-13 09:31:56 -0400

    created SI folders

---
+++ week0/pokemonfunctions.js
@@ -0,0 +1,7 @@
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+function randLetter(){
+    return letters;
+}
+
+console.log (randLetter(letters.length));
+
