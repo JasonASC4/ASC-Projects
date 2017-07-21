c381341bdf7d0a3bcb2dada6de743e1dc37dee1f
JasonASC4 <Jasonhr781@gmail.com>
2017-07-21 15:37:51 -0400

    week1

--- week1/p5/jason-pokeBattle/battle.js
+++ week1/p5/jason-pokeBattle/battle.js
@@ -1,57 +1,3 @@
-var Mewtwo = {
-    "name": "Mewtwo",
-    'type': "psychic",
-    "HP": 106,
-    "def": 90,
-    "atk": 110,
-    "legend": true
-};
-
-var Electabuzz = {
-    "name": "Electabuzz",
-    'type': "electric",
-    "HP": 65,
-    "def": 57,
-    "atk": 83,
-    "legend": false
-};
-
-var Magmar = {
-    "name": "Magmar",
-    'type': "Fire",
-    "HP": 106,
-    "def": 90,
-    "atk": 110,
-    "legend": true
-};
-
-var Swampert = {
-    "name": "Swampert",
-    'type': "Water",
-    "HP": 100,
-    "def": 90,
-    "atk": 110,
-    "legend": false
-};
-
-var Celebi = {
-    "name": "Celebi",
-    'type': "Physchic, Grass",
-    "HP": 100,
-    "def": 100,
-    "atk": 100,
-    "legend": true
-};
-
-var Rayquaza = {
-    "name": "Rayquaza",
-    'type': "dragon",
-    "HP": 105,
-    "def": 90,
-    "atk": 150,
-    "legend": true
-};
-
 var pokemons = [Mewtwo, Electabuzz, Magmar, Swampert, Celebi, Rayquaza];
 
 function setup() {
@@ -88,7 +34,7 @@ function draw() {
 
     fill('black');
     textSize(18);
-    text("HP : " + (Celebi.HP), 265, 85); // hp bottom
+    text("HP : " + Celebi.HP, 265, 85); // hp bottom
 
     fill('black');
     textSize(18);
---
+++ week1/p5/jason-pokeBattle/pokemon.js
@@ -0,0 +1,53 @@
+var Mewtwo = {
+    "name": "Mewtwo",
+    'type': "psychic",
+    "HP": 106,
+    "def": 90,
+    "atk": 110,
+    "legend": true
+};
+
+var Electabuzz = {
+    "name": "Electabuzz",
+    'type': "electric",
+    "HP": 65,
+    "def": 57,
+    "atk": 83,
+    "legend": false
+};
+
+var Magmar = {
+    "name": "Magmar",
+    'type': "Fire",
+    "HP": 106,
+    "def": 90,
+    "atk": 110,
+    "legend": true
+};
+
+var Swampert = {
+    "name": "Swampert",
+    'type': "Water",
+    "HP": 100,
+    "def": 90,
+    "atk": 110,
+    "legend": false
+};
+
+var Celebi = {
+    "name": "Celebi",
+    'type': "Physchic, Grass",
+    "HP": 100,
+    "def": 100,
+    "atk": 100,
+    "legend": true
+};
+
+var Rayquaza = {
+    "name": "Rayquaza",
+    'type': "dragon",
+    "HP": 105,
+    "def": 90,
+    "atk": 150,
+    "legend": true
+};
